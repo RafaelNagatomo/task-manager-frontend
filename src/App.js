74088f92cd3d8638014819c90859393ad522bd8d
Rafael Nagatomo <rafaelnagatomo@gmail.com>
2024-03-21 00:38:21 -0300

    feat: fetch tasks from back-end

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import { useState } from "react"
-import TaskItem from "./components/TaskItem"
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+import TaskItem from "./components/TaskItem";
 
 const App = () => {
-  const [tasks, setTasks] = useState([
-    {
-      id: "1",
-      description: "Estudar Programação",
-      isCompleted: false,
-    },
-    {
-      id: "2",
-      description: "Ler 30 páginas",
-      isCompleted: true,
-    },
-  ])
+    const [tasks, setTasks] = useState([
+        {
+            id: "1",
+            description: "Estudar Programação",
+            isCompleted: false,
+            date: Date.now(),
+        },
+        {
+            id: "2",
+            description: "Ler 30 páginas",
+            isCompleted: true,
+            date: Date.now(),
+        },
+    ]);
 
-  return (
-    <>
-      {tasks.map((task) => (<TaskItem key={task.id} task={task}/>))}
-    </>
-  )
-}
+    const fetchTasks = async () => {
+        try {
+            const { data } = await axios.get("http://localhost:3001");
+
+            setTasks(data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        fetchTasks();
+    }, []);
+
+    return (
+        <>
+            {tasks.map((task) => (
+                <TaskItem key={task.id} task={task} />
+            ))}
+        </>
+    );
+};
 
 export default App;
--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -1,10 +1,15 @@
-const TaskItem = ({task}) => {
+import moment from "moment";
+import "moment/locale/pt-br";
+
+const TaskItem = ({ task }) => {
     return (
         <>
-            <h1>{task.description}</h1>
-            <p>{task.isCompleted ? "Completa" : "Não completa" }</p>
+            <h1>
+                {task.task} {moment(task.date).format(" - (ddd) DD/MM/YYYY")}
+            </h1>
+            <p>{task.isCompleted ? "Completa" : "Não completa"}</p>
         </>
-    )
-}
+    );
+};
 
-export default TaskItem
+export default TaskItem;
