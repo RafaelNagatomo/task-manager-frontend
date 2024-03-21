20407d93d249683322c64ae4242d4fbb44474497
Rafael Nagatomo <rafaelnagatomo@gmail.com>
2024-03-21 02:44:22 -0300

    feat: task component added and stylized

---
+++ src/components/Tasks.jsx
@@ -0,0 +1,52 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+import "./Tasks.scss";
+
+const Tasks = () => {
+    const [tasks, setTasks] = useState([]);
+
+    const fetchTasks = async () => {
+        try {
+            const { data } = await axios.get("http://localhost:3001");
+
+            setTasks(data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        fetchTasks();
+    }, []);
+
+    return (
+        <div className="tasks-container">
+            <h2>Minhas Tarefas</h2>
+
+            <div className="last-tasks">
+                <h3>Últimas Tarefas</h3>
+                <div className="tasks-list">
+                    {tasks
+                        .filter((task) => task.isCompleted === false)
+                        .map((lastTask) => (
+                            <p>{lastTask.task}</p>
+                        ))}
+                </div>
+            </div>
+
+            <div className="completed-tasks">
+                <h3>Tarefas Concluídas</h3>
+                <div className="tasks-list">
+                    {tasks
+                        .filter((task) => task.isCompleted)
+                        .map((completedTask) => (
+                            <p>{completedTask.task}</p>
+                        ))}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Tasks;
